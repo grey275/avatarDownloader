dac8e0b652e2866407db3bd4e9ff442b42ae22d7
grey275 <pjvanderpol@gmail.com>
2019-05-06 18:32:49 -0700

    implemented downloadContributorAvatars

--- src/download_avatars.js
+++ src/download_avatars.js
@@ -1,21 +1,25 @@
 const request = require('request');
 const fs = require('fs');
+const private = require('../private.json');
 
 // const user = 'grey275';
 // const repo = 'requestIntro';
 
-const user = 'jquery';
-const repo = 'jquery';
-
-const TOKEN = JSON.stringify(fs.ReadStream('./private.json')).github_token
+const TOKEN = private.new_token;
 const API_ROOT = 'https://api.github.com';
 
-function getRepoContributors(owner, repo) {
+const request_options = {
+  headers: {
+    'User-Agent': 'request',
+    Authorization: 'Bearer ' + TOKEN,
+  }
+};
+
+function getRepoContributors(owner, repo, cb) {
+  console.log(TOKEN)
   const options = {
+    ...request_options,
     url: `${API_ROOT}/repos/${owner}/${repo}/contributors`,
-    headers: {
-      'User-Agent': 'request'
-    }
   }
 
   console.log(`url: ${options.url})`);
@@ -23,12 +27,36 @@ function getRepoContributors(owner, repo) {
   request(options, (err, response, body) => {
     if (err) {
       console.log(err);
-      return;
+      return
     }
-    console.log(`body: ${body}`);
+    return cb(JSON.parse(body));
   });
 }
 
-getRepoContributors(user, repo);
+function getContributorAvatarURLs(body) {
+  return body.map((contributor) => contributor.avatar_url );
+}
+
+function downloadImageByURL(url, filePath) {
+    const options = {
+      ...request_options,
+      url: url
+    };
+    request
+      .get(options)
+      .pipe(fs.createWriteStream(filePath));
+}
+
+function downloadContributorAvatars(body) {
+  const urls = getContributorAvatarURLs(body);
+  urls.forEach((url, index) => {
+    downloadImageByURL(url, `${index}.jpeg`);
+  })
+}
 
 
+const user = 'jquery';
+const repo = 'jquery';
+
+
+getRepoContributors(user, repo, downloadContributorAvatars);
